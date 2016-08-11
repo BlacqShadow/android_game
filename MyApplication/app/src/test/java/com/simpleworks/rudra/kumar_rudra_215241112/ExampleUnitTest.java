eca6a631559f15acc681e6ffc20a2e4f6dbfea9c
Blacq_Shadow <rudrak1996@gmail.com>
2016-08-11 23:39:46 +1000

    Added the auto scaling UI v0.1

---
+++ MyApplication/app/src/test/java/com/simpleworks/rudra/kumar_rudra_215241112/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.simpleworks.rudra.kumar_rudra_215241112;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
